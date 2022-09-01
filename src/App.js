4d7e72ab94e5c8b9ada87200f1389cc8c0af532b
mina <sadeghbayan.mina@gmail.com>
2022-09-01 10:07:14 +0430

    Header and main nav added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Products from "./Components/Products/Products";
+import Header from "./Containers/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <Products />
       </header>
     </div>
---
+++ src/Components/MainNav/MainNav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MainNav = () => {
+  return (
+    <div>
+      <span>LOGO</span>
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>Return</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MainNav;
