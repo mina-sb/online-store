58c10cff2b58d0e6569c6a2c3d47f8bc72ccc289
mina <sadeghbayan.mina@gmail.com>
2022-09-27 12:01:36 +0330

    banner added

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Banner.scss";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img
+        className="img"
+        src="https://cdn.shopify.com/s/files/1/0304/5739/7386/files/slide1.3.jpg?v=1613680270"
+      />
+      <div className="title">
+        <h3>TREND SHOPPING</h3>
+        <h1>Health Clay Studio</h1>
+        <h2>DinnersetWare</h2>
+        <a className="btn">Shop Now</a>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../../Components/Banner/Banner";
 import MainNav from "../../Components/MainNav/MainNav";
 
 const Header = () => {
   return (
     <div>
       <MainNav />
+      <Banner />
     </div>
   );
 };
