ca31aa16e036debddb4154725f7f117003a0c829
mina <sadeghbayan.mina@gmail.com>
2022-10-05 11:38:12 +0330

    some changes for when cart is empty

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,6 +3,7 @@ import "./Cart.scss";
 import { VscChromeClose } from "react-icons/vsc";
 import { AppContext } from "../../AppContext";
 import CartItem from "../CartItem/CartItem";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartContext, showCart, backdrop, cartTotalPrice } =
@@ -11,6 +12,7 @@ const Cart = () => {
   const [backdropState, setbackdropState] = backdrop;
   const [cart, setCart] = cartContext;
   const [totalPrice, setTotalPrice] = cartTotalPrice;
+  const navigate = useNavigate();
 
   const closeCart = () => {
     setCartState(false);
@@ -30,19 +32,29 @@ const Cart = () => {
         <span className="cart-title">SHOPPING CART</span>
         <span className="cart-counter">{cart.length}</span>
       </div>
-      {cart.map((item, index) => {
-        return (
-          <div key={index}>
-            <CartItem
-              title={item.title}
-              price={item.price}
-              img={item.img}
-              id={item.id}
-              count={item.count}
-            />
-          </div>
-        );
-      })}
+      {cart.length == 0 ? (
+        <div className="empty-cart">
+          <span className="empty-cart-span">Your shopping bag is empty</span>
+          <br />
+          <a className="empty-cart-btn" onClick={closeCart}>
+            Go To The Shop
+          </a>
+        </div>
+      ) : (
+        cart.map((item, index) => {
+          return (
+            <div key={index}>
+              <CartItem
+                title={item.title}
+                price={item.price}
+                img={item.img}
+                id={item.id}
+                count={item.count}
+              />
+            </div>
+          );
+        })
+      )}
       <div className="cart-bottom-reletive">
         <div className="cart-bottom">
           <div className="cart-total">
