5c585bcd359a21a50607f0cda0b1b5e33de14de1
mina <sadeghbayan.mina@gmail.com>
2022-10-06 16:43:32 +0330

    register and forget password added

--- src/Components/Backdrop/Backdrop.js
+++ src/Components/Backdrop/Backdrop.js
@@ -3,15 +3,18 @@ import { AppContext } from "../../AppContext";
 import "./Backdrop.scss";
 
 const Backdrop = () => {
-  const { showCart, backdrop, searchState } = useContext(AppContext);
+  const { showCart, backdrop, searchState, loginState } =
+    useContext(AppContext);
   const [cartState, setCartState] = showCart;
   const [backdropState, setbackdropState] = backdrop;
   const [search, setSearch] = searchState;
+  const [showLogin, setShowLogin] = loginState;
 
   const closeBackdrop = () => {
     setCartState(false);
     setbackdropState(false);
     setSearch(false);
+    setShowLogin(false);
   };
   return <div className="backdrop" onClick={closeBackdrop}></div>;
 };
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,24 +1,57 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../../AppContext";
 import "./Login.scss";
 
 const Login = () => {
   const { loginState } = useContext(AppContext);
-  const [showLogin, setShowLogin] = loginState;
+  const [showForm, setShowForm] = loginState;
+  const [register, setRegister] = useState(1);
 
   return (
-    <div className={`login ${showLogin ? "login-show" : ""}`}>
-      <h3 className="login-title">LOGIN</h3>
-      <hr className="hr" />
-      <h4 className="login-sub-title">Great to have you back!</h4>
-      <input placeholder="Email Adress" className="login-input" />
-      <br />
-      <input placeholder="Password" className="login-input" />
-      <span className="forget-password">Forgot your password?</span>
-      <a className="login-btn">Log In</a>
-      <a className="register-btn">
-        Don’t have an account?<span>Register now</span>
-      </a>
+    <div className={`form ${showForm ? "form-show" : ""}`}>
+      {register == 1 ? (
+        <div className="container">
+          <h3 className="form-title">LOGIN</h3>
+          <hr className="hr" />
+          <h4 className="form-sub-title">Great to have you back!</h4>
+          <input placeholder="Email Adress" className="form-input" />
+          <br />
+          <input placeholder="Password" className="form-input" />
+          <span className="forget-password" onClick={() => setRegister(3)}>
+            Forgot your password?
+          </span>
+          <a className="form-btn">Log In</a>
+          <a className="form-second-btn">
+            Don’t have an account?
+            <span onClick={() => setRegister(2)}>Register now</span>
+          </a>
+        </div>
+      ) : register == 2 ? (
+        <div className="container">
+          <h3 className="form-title">REGISTER</h3>
+          <hr className="hr" />
+          <h4 className="form-sub-title">We Are Happy To See You!</h4>
+          <input placeholder="Email Adress" className="form-input" />
+          <br />
+          <input placeholder="Password" className="form-input" />
+          <a className="form-btn">Register</a>
+          <a className="form-second-btn">
+            <span onClick={() => setRegister(1)}>go back to login</span>
+          </a>
+        </div>
+      ) : (
+        <div className="container">
+          <h3 className="form-title">PASSWORD RESET</h3>
+          <hr className="hr" />
+          <h4 className="form-sub-title">We Help You Back!</h4>
+          <input placeholder="Email Adress" className="form-input" />
+          <br />
+          <a className="form-btn">Submit</a>
+          <a className="form-second-btn">
+            <span onClick={() => setRegister(1)}>go back to login</span>
+          </a>
+        </div>
+      )}
     </div>
   );
 };
