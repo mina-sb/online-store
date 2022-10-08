257e93563603562b39b3bf337f21673fb17ead34
mina <sadeghbayan.mina@gmail.com>
2022-10-08 11:51:32 +0330

    footer minor fixes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,17 +6,17 @@ const Footer = () => {
   return (
     <div className="footer">
       <hr className="hr" />
-      <div className="container">
+      <div className="footer-container">
         <div className="contact">
           <span className="logo">LOGO</span>
           <p>Be the first who learns about our great promotions!</p>
           <span>FOLLOW US</span>
           <hr className="hr" />
           <div className="socialMedia">
-            <FaTwitter className="share-btns" />
-            <FaFacebookF className="share-btns" />
-            <FaPinterestP className="share-btns" />
-            <FaTwitter className="share-btns" />
+            <FaTwitter className="footer-share-btns" />
+            <FaFacebookF className="footer-share-btns" />
+            <FaPinterestP className="footer-share-btns" />
+            <FaTwitter className="footer-share-btns" />
           </div>
         </div>
         <div className="our-product">
